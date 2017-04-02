f7195347743185bcc2fbdd8f5086d594d6f1b1b9
hughest64 <hughest64@gmail.com>
2017-04-02 12:34:46 -0500

    Add files via upload
    Initial commit

---
+++ app.py
@@ -0,0 +1,88 @@
+from flask import (Flask, render_template, redirect,
+                    url_for, request, make_response)
+import random
+import os
+import json
+from brewing_tools import BeerXMLParser
+
+app = Flask(__name__)
+# Timer object for setting our xml data
+recipe_data = BeerXMLParser()
+# may move this locally to the recipes view
+FPATH = os.getcwd() +'\\recipes\\'
+# default value has to be a dict with some value
+recipe_name = {'recipe':"no recipe"}
+
+def new_url():
+    query = random.randint(100000000, 999999999)
+    return '?q={}'.format(query)
+
+
+def get_xml(name):
+    try:
+        path = FPATH + name + ".xml"
+        recipe_data.set_XML(path)
+        all_steps = recipe_data.get_all_steps()
+        recipe = recipe_data.get_recipe_name()
+        all_steps['recipe_name'] = recipe
+        boil_time = recipe_data.get_boil_time()
+        all_steps['boil_time'] = boil_time
+
+    except IOError:
+        all_steps = {}
+
+    return all_steps
+
+
+@app.route('/')
+def index():
+    query = new_url()
+    extension = {'query':query}
+    data = get_xml(recipe_name['recipe'])
+
+    return render_template('index.html', data=data, **extension)
+
+
+@app.route('/set')
+def set():
+    query = new_url()
+    return render_template('set.html', query=query)
+
+
+@app.route('/timer')
+def timer():
+    query = new_url()
+    data = get_xml(recipe_name['recipe'])
+    extension = {'query':query, 'data':data}
+    return render_template('timer.html', **extension)
+
+
+@app.route('/recipes')
+def load_recipes():
+    query = new_url()
+    all_files = os.listdir(FPATH)
+    recipes = []
+    # we only populate .xml files
+    for recipe in all_files:
+        if recipe.split('.')[-1] == 'xml':
+            name = '.'.join(recipe.split('.')[:-1])
+            recipes.append(name)
+    return render_template('recipes.html', recipes=recipes, query=query)
+
+
+@app.route('/saverecipe', methods=['POST'])
+def saverecipe():
+    response = make_response(redirect(url_for('timer')))
+    # we make this global so it is accesible in index function
+    global recipe_name
+    # key is 'recipe'
+    recipe_name = dict(request.form.items())
+    return response
+
+
+
+
+
+
+
+app.run(debug=True, port=8000, host='0.0.0.0')
---
+++ static/timer.js
@@ -0,0 +1,142 @@
+var hr = document.getElementById('hr');
+var mn = document.getElementById('mn');
+var sec = document.getElementById('sec');
+var display = document.getElementById('timer');
+var recipeName = document.getElementById('recipeName');
+var current_step = document.getElementById('current_step');
+var steps = localStorage['steps'];
+var hop_adds = localStorage['hop_adds'];
+var hopKeys = Object.keys(JSON.parse(hop_adds));
+
+
+function hopCheck(time) {
+    var sec = Math.floor(time / 1000);
+    if (current_step.innerHTML == 'Boil' &&
+        hopKeys.indexOf(sec.toString()) != -1) {
+        alert(JSON.parse(hop_adds)[sec]);
+    }
+}
+
+function setTimer() {
+    // if a timer is running, stop it
+    stopTimer();
+    // we only set the timer if there is some value to set
+    if (hr.value || mn.value || sec.value) {
+        // the 450 adds a bump to ms and keeps the timer from skipping
+        var remainingTime = (
+            (Math.abs(Math.floor(hr.value)) * 3600000) +
+            (Math.abs(Math.floor(mn.value)) * 60000) +
+            (Math.abs(Math.floor(sec.value * 1000))) + 450
+        );
+        var resetTime = remainingTime;
+        localStorage['remainingTime'] = remainingTime;
+        localStorage['resetTime'] = resetTime;
+        formatDisplay(remainingTime);
+    }
+}
+
+function runTimer() {
+    if (localStorage['isRunning']) {
+        var endTime = Number(localStorage['endTime']);
+        var remainingTime = endTime - Date.now();
+
+        hopCheck(remainingTime);
+        // adding some buffer time to make sure we don't lose any time
+        if (remainingTime % 1000 < 10) {
+            endTime += 441;
+            localStorage['endTime'] = endTime;
+        }
+
+        localStorage['remainingTime'] = remainingTime;
+        formatDisplay(remainingTime);
+
+        if (remainingTime > 999) {
+            timer = setTimeout(runTimer, 1000);
+        } else {
+            // make values falsey
+            localStorage['isRunning'] = '';
+            localStorage['remainingTime'] = '';
+            localStorage['resetTime'] = '';
+            nextTimer();
+        }
+    }
+}
+
+function formatDisplay(t) {
+    var h = Math.floor(t/3600000);
+    var m = Math.floor(t/60000) % 60;
+    var s = Math.floor(t/1000) % 60;
+
+    var displayHr = "0" + h;
+    var displayMn = "0" + m;
+    var displaySec = "0" + s;
+
+    if (h > 0) {
+        display.innerHTML = displayHr.slice(-2) + ":" + displayMn.slice(-2) + ":" + displaySec.slice(-2);
+    } else {
+        display.innerHTML = displayMn.slice(-2) + ":" + displaySec.slice(-2);
+    }
+}
+
+function startTimer() {
+    if (!localStorage['isRunning'] && localStorage['remainingTime']) {
+        localStorage['isRunning'] = 'true';
+        var remainingTime = Number(localStorage['remainingTime'])
+        var endTime = Date.now() + remainingTime;
+        localStorage['endTime'] = endTime;
+        runTimer();
+    }
+}
+
+function stopTimer() {
+    localStorage['isRunning'] = '';
+    clearTimeout(timer);
+}
+
+function resetTimer() {
+    stopTimer();
+    var remainingTime = localStorage['resetTime'];
+    localStorage['remainingTime'] = remainingTime;
+    formatDisplay(remainingTime);
+}
+
+function clearTimer() {
+    stopTimer();
+    localStorage['remainingTime'] = '';
+    localStorage['resetTime'] = '';
+    localStorage['endTime'] = '';
+    var display = document.getElementById('timer').innerHTML = '00:00';
+}
+
+function nextTimer() {
+    var index = Number(localStorage['stepIndex']);
+    var parsed = JSON.parse(steps);
+    index ++;
+    if (localStorage['stepIndex'] && index < (parsed.length)) {
+        stopTimer();
+        current_step.innerHTML = parsed[index][0];
+        mn.value = (parsed[index][1])
+        localStorage['stepIndex'] = index;
+        setTimer();
+    }
+}
+// deprecate  this outter if/else!!!
+if (!localStorage['newTimer']) {
+    if (localStorage['recipeLoaded']) {
+        recipeName.innerHTML = localStorage['recipeName']
+        nextTimer();
+        localStorage['recipeLoaded'] = '';
+
+    } else {
+        formatDisplay(localStorage['remainingTime']);
+        var index = Number(localStorage['stepIndex'])
+        var parsed = JSON.parse(steps);
+        recipeName.innerHTML = localStorage['recipeName']
+        current_step.innerHTML = parsed[index][0];
+        runTimer();
+    }
+
+} else {
+    localStorage['newTimer'] = '';
+    formatDisplay(localStorage['remainingTime']);
+}
