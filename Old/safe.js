c3cf735c0ed00465eb67a7a59594a5ad54dd10f1
hughest64 <hughest64@gmail.com>
2017-04-12 16:46:49 -0500

    removing files from old/ directory

--- Old/safe.js
+++
@@ -1,6 +0,0 @@
-<script>
-    var steps = JSON.stringify({{ data['mash'] | tojson }})
-    localStorage['steps'] = steps;
-    localStorage['recipeLoaded'] = "true"
-</script>
-<script src="/static/script.js{{ query }}"></script>
