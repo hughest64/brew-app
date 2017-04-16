4d8d2695a7cfb5008da69186eccc68f009fa4827
hughest64 <hughest64@gmail.com>
2017-04-16 10:29:45 -0500

    refactored CSS and made some style changes

--- static/script.js
+++ static/script.js
@@ -1,11 +1,3 @@
-/*var steps = JSON.parse(localStorage['steps']);
-var index = localStorage['stepIndex'];
-var hr = document.getElementById('hr');
-var mn = document.getElementById('mn');
-var sec = document.getElementById('sec');
-console.log(steps[0][1]);
-mn.value = Number(steps[0][1]);*/
-
 function setNewTimer() {
     if (hr.value || mn.value || sec.value) {
         // the 450 ds a bump to ms and keeps the timer from skipping
